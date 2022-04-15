42a5595feee32101026e6220a033f13a3f673576
Everton Ferens <evertonferens0@gmail.com>
2022-04-15 09:18:35 -0300

    Order ,instant two

--- src/main/java/com/crudspring/entities/Order.java
+++ src/main/java/com/crudspring/entities/Order.java
@@ -1,6 +1,7 @@
 package com.crudspring.entities;
 
-import org.aspectj.weaver.ast.Or;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -8,16 +9,19 @@ import java.time.Instant;
 import java.util.Objects;
 
 @Entity
-@Table(name ="tb_order")
+@Table(name = "tb_order")
 public class Order implements Serializable {
     private static final long SerialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
 // UM PEDIDO TEM 1 CLIENTE CRIANDO
+
 
     @ManyToOne
     @JoinColumn(name = "client_id")
