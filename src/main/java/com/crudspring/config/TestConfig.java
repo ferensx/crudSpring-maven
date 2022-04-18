1a81277e3bebbf12d42aadef461f5a9d95f30d82
Everton Ferens <evertonferens0@gmail.com>
2022-04-18 07:41:28 -0300

    Product

--- src/main/java/com/crudspring/config/TestConfig.java
+++ src/main/java/com/crudspring/config/TestConfig.java
@@ -3,11 +3,13 @@ package com.crudspring.config;
 
 import com.crudspring.entities.Category;
 import com.crudspring.entities.Order;
+import com.crudspring.entities.Product;
 import com.crudspring.entities.User;
 
 import com.crudspring.entities.enums.OrderStatus;
 import com.crudspring.repositories.CategoryRepository;
 import com.crudspring.repositories.OrderRepository;
+import com.crudspring.repositories.ProductRepository;
 import com.crudspring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -37,7 +42,15 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
-        categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -45,10 +58,10 @@ public class TestConfig implements CommandLineRunner {
 
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT ,u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT ,u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         userRepository.saveAll(Arrays.asList(u1, u2));
 
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
--- src/main/java/com/crudspring/entities/Category.java
+++ src/main/java/com/crudspring/entities/Category.java
@@ -2,7 +2,9 @@ package com.crudspring.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -10,11 +12,13 @@ public class Category implements Serializable {
     private static final long SerialVersionUID = 1L;
 
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
 
@@ -40,6 +44,9 @@ public class Category implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    public Set<Product> getProducts() {
+        return products;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -53,4 +60,8 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
+
+
 }
