1382475f7c6385e150d01cd907cc7518154cb522
Everton Ferens <evertonferens0@gmail.com>
2022-04-20 09:22:47 -0300

    User Update

--- src/main/java/com/crudspring/resources/UserResource.java
+++ src/main/java/com/crudspring/resources/UserResource.java
@@ -31,12 +31,13 @@ public class UserResource {
     //END POINT
     @GetMapping(value = "/{id}")
 
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+//INSERINDO USARIO
     @PostMapping
-    public ResponseEntity<User> insert (@RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
                 buildAndExpand(obj.getId()).toUri();
@@ -44,10 +45,18 @@ public class UserResource {
 
 
     }
+//DELETANDO USUARIO
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void>delete (@PathVariable Long id ){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+//MUDANDO RECURSOS DO USARIO
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id,obj);
+        return ResponseEntity.ok().body(obj);
+
     }
 
 
--- src/main/java/com/crudspring/services/UserService.java
+++ src/main/java/com/crudspring/services/UserService.java
@@ -14,7 +14,7 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repository.findAll();
     }
 
@@ -23,12 +23,28 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.get();
     }
-    public User insert (User obj){
-      return   repository.save(obj);
+
+    public User insert(User obj) {
+        return repository.save(obj);
 
     }
-    public void delete (Long id){
+
+    public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.getOne(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+
     }
 
 
