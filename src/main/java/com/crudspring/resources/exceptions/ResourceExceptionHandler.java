6d87c10c5491567875801fee5537e3c011f1d684
Everton Ferens <evertonferens0@gmail.com>
2022-04-20 10:06:44 -0300

    Exception handling  delet

--- src/main/java/com/crudspring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/crudspring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.crudspring.resources.exceptions;
 
+import com.crudspring.services.exceptions.DataBaseException;
 import com.crudspring.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,18 @@ public class ResourceExceptionHandler {
 
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(),
+                error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+
+
+    }
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request) {
+
+        String error = "database ERROR";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(),
                 error, e.getMessage(), request.getRequestURI());
 
--- src/main/java/com/crudspring/services/UserService.java
+++ src/main/java/com/crudspring/services/UserService.java
@@ -2,8 +2,11 @@ package com.crudspring.services;
 
 import com.crudspring.entities.User;
 import com.crudspring.repositories.UserRepository;
+import com.crudspring.services.exceptions.DataBaseException;
 import com.crudspring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,7 +34,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+           throw new DataBaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
